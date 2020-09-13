1ef4aef0c7f6ebc10d4aa24c49d3b464c0214e65
mdeng1110 <maydeng1110@yahoo.com>
2020-09-12 22:25:55 -0700

    deleted a file

--- 4_S2_Length_of_Last_Word.py
+++
@@ -1,8 +0,0 @@
-def lengthOfLastWord(string):
-	if " " in string:
-	  word = string.split(" ")[-1]
-	  return len(word)
-	else:
-	  return 0
-sampleInput = "Hello World"
-print(lengthOfLastWord(sampleInput))
